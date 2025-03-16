f15d944f863d73c73f77a117f817faf2f95b5d15
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2025-03-16 12:01:28 +0300

    Update ex007.py

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,8 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 230543606
+# Your Name and Surname: Saed Abdul Rahim
+# ex7.py
+string = input("Enter a string: ")
+frequency = {char: string.count(char) for char in sorted(set(string))}
+print("Character frequencies:")
+for char, count in frequency.items():
+    print(f"{char}: {count}")
