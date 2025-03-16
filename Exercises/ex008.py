72ea696aa99a5a4ead4d34321404077244466280
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2025-03-16 12:02:22 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,6 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 230543606
+# Your Name and Surname: Saed Abdul Rahim
+# ex8.py
+n = int(input("Enter the number of rows: "))
+for i in range(1, n+1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
